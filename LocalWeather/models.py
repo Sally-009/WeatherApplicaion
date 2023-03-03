3c8b639d066796ae6b28cd6425253c580ebfbf45
Sally <skojima2025@gmail.com>
2023-03-02 22:37:29 -0600

    Add files via upload

--- LocalWeather/models.py
+++ LocalWeather/models.py
@@ -4,4 +4,10 @@ import json
 
 class currentConditions():
     url = "https://api.weather.gov/gridpoints/LZK/35,93/forecast"
+    # the entire data of this API
     returnData = requests.get(url).json()
+    # properties in the entire data
+    properties = returnData['properties']
+    # periods in properties
+    periods = properties['periods']
+    
--- LocalWeather/views.py
+++ LocalWeather/views.py
@@ -5,9 +5,14 @@ from .models import currentConditions
 
 # Create your views here.
 def index(request):
-    dataPage = currentConditions.returnData
-    template = loader.get_template('home.html')
+    dataPage = currentConditions.returnData # the whole data
+    prop = currentConditions.properties     # properties
+    periods = currentConditions.periods     # periods
+    
+    template = loader.get_template('home.html') # HTTP template
     context = {
         'dataPage': dataPage,
+        'properties' : prop,
+        'periods' : periods,
     }
     return HttpResponse(template.render(context, request))
